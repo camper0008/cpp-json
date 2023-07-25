3562eb79111d07679eccb85b8ada37a4f579220c
SimonFJ20 <simonfromjakobsen@gmail.com>
2023-07-25 23:25:56 +0200

    format files indenting 4

--- lexer.hpp
+++ lexer.hpp
@@ -4,51 +4,51 @@
 #include <string>
 
 enum class TokenType {
-   RBrace,
-   LBrace,
-   RBracket,
-   LBracket,
-   Int,
-   Decimal,
-   String,
-   Colon,
-   Comma,
-   True,
-   False,
-   Null,
-   Eof,
+    RBrace,
+    LBrace,
+    RBracket,
+    LBracket,
+    Int,
+    Decimal,
+    String,
+    Colon,
+    Comma,
+    True,
+    False,
+    Null,
+    Eof,
 };
 
 struct Token {
-   TokenType type;
-   size_t position;
-   size_t length;
-   size_t line;
-   size_t column;
+    TokenType type;
+    size_t position;
+    size_t length;
+    size_t line;
+    size_t column;
 };
 
 class Lexer {
-   private:
-   std::string text;
-   size_t position = 0;
-   size_t line = 1;
-   size_t column = 1;
+private:
+    std::string text;
+    size_t position = 0;
+    size_t line = 1;
+    size_t column = 1;
 
-   public:
-   Lexer(std::string text)
-       : text(std::move(text))
-   {
-   }
+public:
+    Lexer(std::string text)
+        : text(std::move(text))
+    {
+    }
 
-   void step();
+    void step();
 
-   auto skip_comment() -> Token;
+    auto skip_comment() -> Token;
 
-   auto number() -> Token;
+    auto number() -> Token;
 
-   auto str() -> Token;
+    auto str() -> Token;
 
-   auto n_token(TokenType type, size_t len) -> Token;
+    auto n_token(TokenType type, size_t len) -> Token;
 
-   auto next() -> Token;
+    auto next() -> Token;
 };
--- main.cpp
+++ main.cpp
@@ -1,42 +1,42 @@
 #include "parser.hpp"
+#include <format>
 #include <iostream>
+
+using namespace std::literals;
 
 auto main() -> int
 {
-   std::string text = R"(
-    {
-      "firstName": "John",
-      "lastName": "Smith",
-      "isAlive": true,
-      "age": 27,
-      "address": {
-        "streetAddress": "21 2nd Street",
-        "city": "New York",
-        "state": "NY",
-        "postalCode": "10021-3100"
-      },
-      "phoneNumbers": [
-        {
-          "type": "home",
-          "number": "212 555-1234"
+    std::string text = R"({
+        "firstName": "John",
+        "lastName": "Smith",
+        "isAlive": true,
+        "age": 27,
+        "address": {
+            "streetAddress": "21 2nd Street",
+            "city": "New York",
+            "state": "NY",
+            "postalCode": "10021-3100"
         },
-        {
-          "type": "office",
-          "number": "646 555-4567"
-        }
-      ],
-      "children": [
-        "Catherine",
-        "Thomas",
-        "Trevor"
-      ],
-      "spouse": null
+        "phoneNumbers": [
+            {
+                "type": "home",
+                "number": "212 555-1234"
+            },
+            {
+                "type": "office",
+                "number": "646 555-4567"
+            }
+        ],
+        "children": [
+            "Catherine",
+            "Thomas",
+            "Trevor"
+        ],
+        "spouse": null
     })";
 
-   using namespace std::string_literals;
-
-   auto parser = Parser(text);
-   auto tree = parser.parse();
-   std::string type = tree["phoneNumbers"s][1]["type"s];
-   std::cout << type << "\n"; // "office"
+    auto parser = Parser(text);
+    auto tree = parser.parse();
+    auto type = std::string(tree["phoneNumbers"s][1]["type"s]);
+    std::cout << std::format("please let '{}' equal 'office'\n", type);
 }
